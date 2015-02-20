6570550c6b5296c6cb131c7012b3e581d7b12cf3
YFC <757320451@qq.com>
2015-02-20 23:42:34 +0800

    first commit

---
+++ HelloWorld/src/com/example/helloworld/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+	private EditText firstText,secondText;
+	private Button btn1,btn2,btn3,btn4,calculate;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	
+	
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
